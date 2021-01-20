f2fee9d46e240d3ae77ddb99c5ab3419b09119ad
UrbanCoffee <rudy.m.7854@gmail.com>
2021-01-20 11:21:29 -0800

    Updates to c-count.h and main.cpp

--- c-count.h
+++ c-count.h
@@ -1,6 +1,18 @@
-#include "c-echo.h"
-#include "c-count.h"
+#include <iostream>
 
-int main(int argv, char** argc) {
-    std::cout << "Count of output: " << count(echo(argv,argc)) << std::endl;
+unsigned int count(const std::string& str) {
+    unsigned int counter = 0;
+    bool on_space = true;
+    
+    for(int i = 0; i < str.size(); i++) {
+        if (std::isspace(str[i]))
+            on_space = true;
+        else if (on_space) {
+            counter++;
+            on_space = false;
+        }
+    }
+
+    return counter;
 }
+
